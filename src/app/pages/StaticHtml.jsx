e68eafe2458932f628a892964cf92e0d1ff2f430
Alberto <a.tiboni@gmail.com>
2023-01-11 19:40:11 +0100

    managing anchor scroll

--- src/app/pages/StaticHtml.jsx
+++ src/app/pages/StaticHtml.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 import { useParams } from 'react-router-dom';
 
 import Template from '../components/template/Template.jsx';
 import MarkdownRenderer from '../components/wrappers/MarkdownRenderer.jsx';
+import { useDidMount } from '../hooks/useDidMount.js';
 
 const StaticHtmlPage = () => {
     const { filename } = useParams();
+
+    const didMount = useDidMount();
+
+    // test hash and scroll to referenced element..
+    useEffect(() => {
+        if (didMount) {
+            const hash = decodeURI(window.location.hash)
+            const element = document.getElementById(hash.replace('#', ''))
+            element && element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+        }
+    }, [didMount]);
 
     return (
         <Template withWrapper>
